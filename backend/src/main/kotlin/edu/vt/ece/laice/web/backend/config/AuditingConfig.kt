60bdcd9364d68556677d354d910bf86f0f69538a
Cameron <camearle20@me.com>
2018-06-19 15:23:56 -0400

    Added user and role date auditing

--- backend/src/main/kotlin/edu/vt/ece/laice/web/backend/config/AuditingConfig.kt
+++ backend/src/main/kotlin/edu/vt/ece/laice/web/backend/config/AuditingConfig.kt
@@ -1,8 +1,28 @@
 package edu.vt.ece.laice.web.backend.config
 
+import edu.vt.ece.laice.web.backend.security.UserPrincipal
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.security.authentication.AnonymousAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import java.util.*
 
 @Configuration
 @EnableJpaAuditing
-class AuditingConfig
+class AuditingConfig {
+    @Bean
+    fun auditorProvider(): AuditorAware<Long> {
+        return AuditorAware<Long> {
+            val authentication = SecurityContextHolder.getContext().authentication
+
+            if (authentication == null || !authentication.isAuthenticated || authentication is AnonymousAuthenticationToken) {
+                Optional.empty<Long>() //return
+            }
+
+            val userPrincipal = authentication.principal as UserPrincipal
+            Optional.ofNullable(userPrincipal.id) //return
+        }
+    }
+}
