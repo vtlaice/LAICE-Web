8d1385cfc82a734b3ba8bc595ac63cce02b2aff4
Cameron <camearle20@me.com>
2018-07-06 18:12:52 -0400

    Updated packetsummaryresponse to make names correct

--- backend/src/main/kotlin/edu/vt/ece/laice/web/backend/payload/PacketSummaryResponse.kt
+++ backend/src/main/kotlin/edu/vt/ece/laice/web/backend/payload/PacketSummaryResponse.kt
@@ -3,9 +3,9 @@ package edu.vt.ece.laice.web.backend.payload
 import java.time.Instant
 
 data class PacketSummaryResponse(val id: Long,
-                                 val start: Instant,
-                                 val end: Instant,
-                                 val title: String,
+                                 val startTime: Instant,
+                                 val endTime: Instant,
+                                 val name: String,
                                  val createdAt: Instant,
                                  val lastModifiedAt: Instant,
                                  val createdBy: Long,
